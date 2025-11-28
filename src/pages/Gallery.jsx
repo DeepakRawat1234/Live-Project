454df0289fe9e39794ddda4d4302614fb2ce335a
deepakrawat1234 <rawatyuvi.2006@gmail.com>
2025-11-27 21:17:41 -0500

    addded more strucuted

---
+++ src/pages/Gallery.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Layout/Navbar'
+
+const Gallery = () => {
+  return (
+    <div>
+        <Navbar/>
+    </div>
+  )
+}
+
+export default Gallery
