db03d430cb84087bc16e27227fb798dd2cdd51e7
CHETANNEGI75 <negichetan10@gmail.com>
2025-11-29 12:29:51 +0530

    Updated work on Chetan branch

---
+++ src/components/Information/InformationHero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/Information.css";
+
+export default function InformationHero() {
+  return (
+    <div className="info-hero">
+      <div className="info-hero-overlay">
+        <h1>
+          India’s culture is not just history — it is created <br />
+          every day, by real people, in real communities.
+        </h1>
+      </div>
+    </div>
+  );
+}
