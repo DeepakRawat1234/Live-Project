8791946b77d405d6e4bb673e9b9f4599852990b0
deepakrawat1234 <rawatyuvi.2006@gmail.com>
2025-11-29 12:55:35 -0500

    Resolved merge conflict in App.jsx on deepak branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
-import Gallery from './pages/Gallery'
-import Signup from './components/login/Signup'
-import Login from './components/login/Login'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import React from 'react';
+import Gallery from './pages/Gallery';
+import Signup from './components/login/Signup';
+import Login from './components/login/Login';
+import Information from './pages/Information';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
-   <BrowserRouter>
-      {/* <{Gallery}/> */}
-      
+    <BrowserRouter>
       <Routes>
-        <Route path='/Login' element={<Login/>}></Route>
-        <Route path='/Signup' element={<Signup/>}></Route>
+        {/* Home Page */}
+        <Route path="/" element={<Gallery />} />
+
+        {/* Information Page */}
+        <Route path="/info" element={<Information />} />
+
+        {/* Login & Signup */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
